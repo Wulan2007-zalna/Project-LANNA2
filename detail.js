fae1d3629edba679c7cb17fe6275071f5de34adc
Wulan2007-zalna <wulannuraini2804@gmail.com>
2025-11-19 23:25:22 +0800

    Update detail.js

--- detail.js
+++ detail.js
@@ -1,10 +1,14 @@
-const dataKota = [
+    const dataKota = [
     {
         nama: "Surabaya",
         ikon: "Tugu Pahlawan",
-        makanan: "Rawon, Rujak Cingur",
-        map: "https://www.google.com/maps/embed?pb=!1m18..."
+        makanan: "Rawon, Rujak Cingur, Lontong Balap, Semanggi Suroboyo",
+        sejarah: "Surabaya adalah ibu kota Jawa Timur dan dikenal sebagai Kota Pahlawan.",
+        julukan: "Kota Pahlawan",
+        map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.7396216637033!2d112.731!3d-7.250445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa0688bffa7b%3A0xfd004d6b7f57e9ec!2sTugu%20Pahlawan!5e0!3m2!1sid!2sid!4v1697030000000"
     },
+
+
     {
         nama: "Malang",
         ikon: "Alun-Alun Tugu",
